49d0b4e9acbadff01ddbb0261a01cf8990a987db
bit <moon.oyds2@gmail.com>
2022-01-17 16:58:31 +0900

    220117 python01

--- 02장 자료형/test.py
+++ 02장 자료형/test.py
@@ -1,2 +1,15 @@
-print("Python Programming")
-print("hello")
+# 사용자로부터 이름과 성적을 입력받아 총점과 평균을 구해 출력하는
+# 프로그램을 작성하시오.
+
+name = input("이름을 입력하세요: ")
+kor = input("국어 점수를 입력하세요: ")
+eng = input("영어 점수를 입력하세요: ")
+math = input("수학 점수를 입력하세요: ")
+
+total = int(kor) + int(eng) + int(math) #str에서 int로 형변환
+avg = total / 3
+
+print(name,"님의 총점=",total,"평균=",avg,"입니다.")
+#print("입력하신 이름은",name,"입니다.")
+#print("평균은",avg,"점 입니다")
+
